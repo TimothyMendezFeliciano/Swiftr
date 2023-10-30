4a2cad62a458256060ce52b4f01d92c454d8433e
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-10-30 16:38:30 -0700

    Emulator Prepared

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAn0pVbikCrNPbxnEh6qEBBA59q6ylEFjQ",
+    authDomain: "swiftpr.firebaseapp.com",
+    databaseURL: "https://swiftpr-default-rtdb.firebaseio.com",
+    projectId: "swiftpr",
+    storageBucket: "swiftpr.appspot.com",
+    messagingSenderId: "958636239852",
+    appId: "1:958636239852:web:01bd2c3db888e23dc2eec9"
+}
